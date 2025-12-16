2d5f8af3204a96e1e108103e6453b41d63b4ab87
TheWoxPL <wojtek.bubula@onet.pl>
2025-12-16 19:55:22 +0100

    feat: add animation to appearing charging window info

--- src/components/ChargingWindow.tsx
+++ src/components/ChargingWindow.tsx
@@ -1,5 +1,6 @@
 import type { ChargingWindowResult } from "@/types";
 import { format } from "date-fns";
+import { motion } from "framer-motion";
 
 interface ChargingWindowProps {
   result: ChargingWindowResult;
@@ -9,8 +10,14 @@ export const ChargingWindow = ({ result }: ChargingWindowProps) => {
   const formatDate = (dateString: string) => {
     return format(new Date(dateString), "dd.MM.yyyy HH:mm");
   };
+
   return (
-    <div className="mt-4 rounded-md border border-slate-200 bg-white sm:p-4 p-2">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="mt-4 rounded-md border border-slate-200 bg-white sm:p-4 p-2"
+    >
       <h3 className="text-sm font-semibold">Recommended charging window (in next 48 hours)</h3>
 
       <dl className="mt-3 grid grid-cols-3 sm:gap-4 gap-2">
@@ -26,9 +33,11 @@ export const ChargingWindow = ({ result }: ChargingWindowProps) => {
 
         <div className="rounded-md bg-slate-50 p-2 sm:w-full text-xs">
           <dt className="font-medium text-slate-500">Avg. clean energy</dt>
-          <dd className="mt-1 text-sm  font-semibold">{result.averageCleanEnergyUsage}%</dd>
+          <dd className="mt-1 text-sm text-emerald-600 font-semibold">
+            {result.averageCleanEnergyUsage}%
+          </dd>
         </div>
       </dl>
-    </div>
+    </motion.div>
   );
 };
